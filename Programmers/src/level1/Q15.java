224ab3f29ca3ac4185d9f41e1789c1ec0adea188
hyeonseo25 <s2019s30@e-mirim.hs.kr>
2021-05-28 20:15:24 +0900

    콜라츠 추측

---
+++ Programmers/src/level1/Q15.java
@@ -0,0 +1,27 @@
+package level1;
+
+// Äİ¶óÃ÷ ÃßÃø
+
+public class Q15 {
+	public static void main(String[] args) {
+		System.out.println(solution(6));
+		System.out.println(solution(16));
+		System.out.println(solution(626331));
+	}
+    public static int solution(long num) {
+        int answer = 0;
+        while(num!=1){
+            if(num%2==0){
+                num/=2;
+            }else{
+                num = num*3+1;
+            }
+            answer++;
+            if(answer>=500){
+                answer = -1;
+                break;
+            }
+        }
+        return answer;
+    }
+}
